43bb9c58059444e9d63850bb43f59ef8c4bc18a4
MacBook Pro <naym100m@gmail.com>
2024-11-15 21:35:33 +0600

    [folder structure] compleate creating a project foulder structure

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/Container";
+
+export default function Home() {
+  return (
+    <>
+      <Container>
+        <p>Home Page</p>
+      </Container>
+    </>
+  );
+}
